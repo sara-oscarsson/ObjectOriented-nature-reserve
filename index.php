8bda9717dd95b945b3a938c5e85450f321ddd2f2
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-01-13 13:27:27 +0100

    form

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nature reserve</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h3>Create your own Nature Reserve:</h3>
+    <form action="" method="post">
+    <input type="number" placeholder= "Number of giraffes" name="giraffe" id="">
+    <input type="number" placeholder= "Number of turtles" name="turtle" id="">
+    <input type="number" placeholder= "Number of lions" name="lion" id="">
+    <button type="submit">Create</button> 
+    </form>
+    
+</body>
+</html>
